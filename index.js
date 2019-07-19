c09627488c4a5622c3cb4f5073ec18b128b36895
debonair-proc-5718 <jeanemma6@gmail.com>
2019-07-19 15:53:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
 function dextructivelyAppendKitten(Milo, Otis, Garfield)
-{}
+{
+  appendKitten()
+}
