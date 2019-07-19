5040525e22e94d36a03a3ae45a84a3b34ffc998f
debonair-proc-5718 <jeanemma6@gmail.com>
2019-07-19 16:21:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function prependKitten(name)
 {
   return [name, ...kittens]
   }
-  function removeLastKitten(Garfield)
+  function removeLastKitten()
   {
     return kittens.slice(0, kittens.lenght-1)
   }
